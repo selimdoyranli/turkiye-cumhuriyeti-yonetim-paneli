9b6a03697fd23ccbdc0b9c1fdde1395f4139c471
selimdoyranli <selim@radkod.com>
2021-12-13 23:51:05 +0300

    :tada: init

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.page.main-page
+  h1 ok
+</template>
+
+<script>
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {}
+})
+</script>
+
+<style lang="scss" scoped>
+.page {
+  position: relative;
+}
+</style>
