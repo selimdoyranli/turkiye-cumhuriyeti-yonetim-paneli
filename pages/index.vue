eee4fbad943f01d6a43a3e88cb82e77f247d0007
selimdoyranli <selim@radkod.com>
2021-12-14 02:42:35 +0300

    :mag: add og

--- pages/index.vue
+++ pages/index.vue
@@ -18,8 +18,11 @@ export default defineComponent({
       {
         hid: 'description',
         name: 'description',
-        content: `Siz bu kardeşinize password'u verin`
-      }
+        content: `Siz bu kardeşinize password'u verin...`
+      },
+      { hid: 'og:title', name: 'og:title', content: 'Türkiye Cumhuriyeti Yönetim Paneli' },
+      { hid: 'og:description', name: 'og:description', content: `Siz bu kardeşinize password'u verin...` },
+      { hid: 'og:image', name: 'og:image', content: `/favicon.ico` }
     ]
   }
 })
